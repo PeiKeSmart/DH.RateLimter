0e306a9cc59829de98cab5f45432c1bc55e4227e
猿人易 <qcjxberin@outlook.com>
2024-12-04 12:16:08 +0800

    重构 ApiThrottleActionFilter 类及其方法
    重命名命名空间为 DH.RateLimter.Filter。
    将 ApiThrottleActionFilter 类从嵌套类更改为顶级类。
    将字段 _api 和 _valves 的注释从中文更改为英文，并将 _api 的类型从 string 更改为 String。
    调整构造函数、OnActionExecutionAsync 和 OnPageHandlerExecutionAsync 方法的逻辑结构和缩进。
    简化 OnPageHandlerSelectionAsync 方法的实现为单行表达式。
    将 HandleAsync 和 CheckAsync 方法的注释从中文更改为英文，并调整其逻辑结构和缩进。

--- DH.RateLimter/Filter/ApiThrottleActionFilter.cs
+++ DH.RateLimter/Filter/ApiThrottleActionFilter.cs
@@ -6,124 +6,120 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 using Pek.Configs;
 
-namespace DH.RateLimter.Filter
+namespace DH.RateLimter.Filter;
+
+public class ApiThrottleActionFilter : IAsyncActionFilter, IAsyncPageFilter
 {
-    public class ApiThrottleActionFilter : IAsyncActionFilter, IAsyncPageFilter
+    private readonly RateLimitProcessor _processor;
+    private readonly RateLimterOptions _options;
+
+    //Api名称
+    private String _api = null;
+    private IEnumerable<Valve> _valves;
+
+    public ApiThrottleActionFilter(RateLimitProcessor processor, RateLimterOptions options)
     {
-        private readonly RateLimitProcessor _processor;
-        private readonly RateLimterOptions _options;
+        _processor = processor;
+        _options = options;
+    }
 
-        //Api名称
-        private string _api = null;
-        private IEnumerable<Valve> _valves;
-
-        public ApiThrottleActionFilter(RateLimitProcessor processor, RateLimterOptions options)
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        if (!RateLimterSetting.Current.AllowRateLimter) await next().ConfigureAwait(false);
+        else
         {
-            _processor = processor;
-            _options = options;
-        }
-
-        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
-        {
-            if (!RateLimterSetting.Current.AllowRateLimter) await next().ConfigureAwait(false);
-            else
-            {
-                var result = await HandleAsync(context).ConfigureAwait(false);
-                if (result.result)
-                {
-                    await next().ConfigureAwait(false);
-                }
-                else
-                {
-                    context.Result = _options.onIntercepted(context.HttpContext, result.valve, IntercepteWhere.ActionFilter);
-                }
-            }
-        }
-
-        public async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
-        {
-            if (!RateLimterSetting.Current.AllowRateLimter) await next().ConfigureAwait(false);
-            else
-            {
-                var result = await HandleAsync(context).ConfigureAwait(false);
-                if (result.result)
-                {
-                    await next().ConfigureAwait(false);
-                }
-                else
-                {
-                    context.Result = _options.onIntercepted(context.HttpContext, result.valve, IntercepteWhere.PageFilter);
-                }
-            }
-        }
-
-        public async Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context)
-        {
-            await Task.CompletedTask.ConfigureAwait(false);
-        }
-
-        /// <summary>
-        /// 处理接口
-        /// </summary>
-        /// <returns></returns>
-        private async Task<(bool result, Valve valve)> HandleAsync(FilterContext context)
-        {
-            //预处理数据
-            var method = context.GetHandlerMethod();
-
-            _api = method.DeclaringType.FullName + "." + method.Name;
-
-            _valves = method.GetCustomAttributes<Valve>(true);
-
-            //检查是否过载
-            var result = await CheckAsync(context).ConfigureAwait(false);
+            var result = await HandleAsync(context).ConfigureAwait(false);
             if (result.result)
             {
-                context.HttpContext.Request.Headers[Common.HeaderStatusKey] = "1";
+                await next().ConfigureAwait(false);
             }
             else
             {
-                context.HttpContext.Request.Headers[Common.HeaderStatusKey] = "0";
+                context.Result = _options.onIntercepted(context.HttpContext, result.valve, IntercepteWhere.ActionFilter);
             }
+        }
+    }
 
-            return result;
+    public async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
+    {
+        if (!RateLimterSetting.Current.AllowRateLimter) await next().ConfigureAwait(false);
+        else
+        {
+            var result = await HandleAsync(context).ConfigureAwait(false);
+            if (result.result)
+            {
+                await next().ConfigureAwait(false);
+            }
+            else
+            {
+                context.Result = _options.onIntercepted(context.HttpContext, result.valve, IntercepteWhere.PageFilter);
+            }
+        }
+    }
+
+    public async Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context) => await Task.CompletedTask.ConfigureAwait(false);
+
+    /// <summary>
+    /// 处理接口
+    /// </summary>
+    /// <returns></returns>
+    private async Task<(Boolean result, Valve valve)> HandleAsync(FilterContext context)
+    {
+        //预处理数据
+        var method = context.GetHandlerMethod();
+
+        _api = method.DeclaringType.FullName + "." + method.Name;
+
+        _valves = method.GetCustomAttributes<Valve>(true);
+
+        //检查是否过载
+        var result = await CheckAsync(context).ConfigureAwait(false);
+        if (result.result)
+        {
+            context.HttpContext.Request.Headers[Common.HeaderStatusKey] = "1";
+        }
+        else
+        {
+            context.HttpContext.Request.Headers[Common.HeaderStatusKey] = "0";
         }
 
-        /// <summary>
-        /// 检查过载
-        /// </summary>
-        /// <returns></returns>
-        private async Task<(bool result, Valve valve)> CheckAsync(FilterContext context)
+        return result;
+    }
+
+    /// <summary>
+    /// 检查过载
+    /// </summary>
+    /// <returns></returns>
+    private async Task<(Boolean result, Valve valve)> CheckAsync(FilterContext context)
+    {
+        //循环验证是否过载
+        foreach (var valve in _valves.OrderByDescending(x => x.Priority))
         {
-            //循环验证是否过载
-            foreach (var valve in _valves.OrderByDescending(x => x.Priority))
+            if (valve is RateValve rateValve)
             {
-                if (valve is RateValve rateValve)
+                //速率阀门
+                if (rateValve.Duration <= 0 || rateValve.Limit <= 0)
                 {
-                    //速率阀门
-                    if (rateValve.Duration <= 0 || rateValve.Limit <= 0)
-                    {
-                        //不限流
-                        continue;
-                    }
+                    //不限流
+                    continue;
+                }
 
-                    //取得识别值
-                    var policyValue = context.HttpContext.GetPolicyValue(_options, valve.Policy, valve.PolicyKey);
+                //取得识别值
+                var policyValue = context.HttpContext.GetPolicyValue(_options, valve.Policy, valve.PolicyKey);
 
-                    // increment counter
-                    //判断是否过载
-                    var rateLimitCounter = await _processor.ProcessRequestAsync(_api, policyValue, valve, context.HttpContext.RequestAborted).ConfigureAwait(false);
+                // increment counter
+                //判断是否过载
+                var rateLimitCounter = await _processor.ProcessRequestAsync(_api, policyValue, valve, context.HttpContext.RequestAborted).ConfigureAwait(false);
 
-                    //XTrace.WriteLine($"获取到的数据：{rateLimitCounter.Count}_{rateLimitCounter.Timestamp}");
+                //XTrace.WriteLine($"获取到的数据：{rateLimitCounter.Count}_{rateLimitCounter.Timestamp}");
 
-                    if (rateLimitCounter.Count > rateValve.Limit)
-                    {
-                        return (false, valve);
-                    }
+                if (rateLimitCounter.Count > rateValve.Limit)
+                {
+                    return (false, valve);
                 }
             }
-
-            return (true, null);
         }
+
+        return (true, null);
     }
 }

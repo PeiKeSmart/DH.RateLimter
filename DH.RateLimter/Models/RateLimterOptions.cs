3b686ce0bb3dab4f51f9251929507391e418e2b5
猿人易 <qcjxberin@outlook.com>
2025-07-01 15:57:34 +0800

    更新 RateLimterOptions.cs 文件
    - 添加对 Microsoft.AspNetCore.Mvc 的引用
    - 更新类 RateLimterOptions 的注释，增加中文描述
    - 修改 OnUserIdentity 和 OnIpAddress 属性的类型声明为 String
    - 保留 onIntercepted 属性的实现，未做修改

--- DH.RateLimter/Models/RateLimterOptions.cs
+++ DH.RateLimter/Models/RateLimterOptions.cs
@@ -1,25 +1,23 @@
-﻿using System;
-using DH.RateLimter.Extensions;
-using Microsoft.AspNetCore.Http;
+﻿using DH.RateLimter.Extensions;
+
 using Microsoft.AspNetCore.Mvc;
 
-namespace DH.RateLimter
+namespace DH.RateLimter;
+
+/// <summary>
+/// 配置项
+/// </summary>
+public class RateLimterOptions
 {
     /// <summary>
-    /// 配置项
+    /// 取得认证用户身份
     /// </summary>
-    public class RateLimterOptions
-    {
-        /// <summary>
-        /// 取得认证用户身份
-        /// </summary>
-        public Func<HttpContext, string> OnUserIdentity = context => context.GetDefaultUserIdentity();
+    public Func<HttpContext, String> OnUserIdentity = context => context.GetDefaultUserIdentity();
 
-        /// <summary>
-        /// 取得客户端IP地址
-        /// </summary>
-        public Func<HttpContext, string> OnIpAddress = context => context.GetIpAddress();
+    /// <summary>
+    /// 取得客户端IP地址
+    /// </summary>
+    public Func<HttpContext, String> OnIpAddress = context => context.GetIpAddress();
 
-        public Func<HttpContext, Valve, IntercepteWhere, IActionResult> onIntercepted = (context, valve, where) => { return new RateLimterResult { Content = "访问过于频繁，请稍后重试！" }; };
-    }
+    public Func<HttpContext, Valve, IntercepteWhere, IActionResult> onIntercepted = (context, valve, where) => { return new RateLimterResult { Content = "访问过于频繁，请稍后重试！" }; };
 }
